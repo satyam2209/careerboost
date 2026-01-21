87e4426da2ab5c372dc1b5cdd4e0e9792dd61e21
Satyam Singh <96168882+satyam2209@users.noreply.github.com>
2026-01-21 11:32:49 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  reactCompiler: true,
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
